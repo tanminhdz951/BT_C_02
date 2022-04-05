a3a237ce3c4fc62bcc1db0ef2f74102782c56cc3
Your Name <you@example.com>
2022-04-05 08:48:35 +0700

    update sua

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApp1
             for (int i = 0; i< n; i++)
             {
                 Console.Clear();
-                Console.WriteLine("Moi nhap thong tin sinh vien thu " + i+1);
+                Console.WriteLine("Moi nhap thong tin sinh vien thu " + (i+1));
                 Console.Write("Nhap Ma SV: ");
                 MANGSV[i].MaSV_236 = int.Parse(Console.ReadLine());
                 Console.Write(" Ho ten: ");
@@ -30,6 +30,7 @@ namespace ConsoleApp1
                 MANGSV[i].DiemLy_236 = Double.Parse(Console.ReadLine());
                 Console.Write(" Diem van: ");
                 MANGSV[i].DiemVan_236 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("");
 
             }
         }
@@ -37,7 +38,7 @@ namespace ConsoleApp1
         static void XuatThongTinSinhVien(SinhVien[] MANGSV)
         {
             Console.Clear();
-            Console.WriteLine("========Thong tin cac sinh vien da nhap la: ");
+            Console.WriteLine("========Thong tin cac sinh vien da nhap la:====== ");
             foreach(SinhVien item in MANGSV){
                 Console.WriteLine(" Ma Sinh Vien: " + item.HoTen_236);
                 Console.WriteLine(" Ho ten: " +item.HoTen_236);
@@ -45,6 +46,29 @@ namespace ConsoleApp1
                 Console.WriteLine(" Diem ly: " + item.DiemLy_236);
                 Console.WriteLine(" Diem van: " + item.DiemVan_236);
                 Console.WriteLine(" Diem trung binh la: " + DiemTBSinhVien(item));
+                if( DiemTBSinhVien(item) > (int)HocLuc.Gioi)
+                {
+                    Console.WriteLine(" Hoc luc gioi ");
+                }
+                else
+                {
+                    if (DiemTBSinhVien(item) > (int)HocLuc.Kha)
+                    {
+                        Console.WriteLine(" Hoc luc kha ");
+                    }
+                    else
+                    {
+                        if (DiemTBSinhVien(item) > (int)HocLuc.TrungBinh)
+                        {
+                            Console.WriteLine(" Hoc luc trung binh ");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Hoc luc kem ");
+                        }
+                    }    
+                }
+               
             }
            
         }
@@ -54,15 +78,20 @@ namespace ConsoleApp1
             return (SV.DiemToan_236 + SV.DiemLy_236 + SV.DiemVan_236) / 3;
         }
 
-        enum Color
+        enum HocLuc
         {
             Kem = 4,
             TrungBinh = 5,
             Kha = 7,
             Gioi = 8
-
         }
 
+        static void timKiemTheoTen(SinhVien[] MANGSV, char ten, int n)
+        {
+           
+
+
+        }
         static void Main(string[] args)
         {
             int n;
@@ -71,7 +100,6 @@ namespace ConsoleApp1
             SinhVien[] MANGSV = new SinhVien[n];
             NhapDSSV(MANGSV, n);
             XuatThongTinSinhVien(MANGSV);
-
             Console.ReadLine();
         }
     }
